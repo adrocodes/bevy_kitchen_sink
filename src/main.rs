95e6f041a8f7b69d6590e450b1110d4fd2aa73ca
adrocodes <adrocodes@users.noreply.github.com>
2023-01-11 20:16:39 +1100

    feat: add pancam and solar system to main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use bevy::{prelude::*, winit::WinitSettings};
 use bevy_mouse_position::MousePositionPlugin;
 
-use command_defender::CommandDefenderPlugin;
+use pancam::{PanCam, PanCamPlugin};
+// use command_defender::CommandDefenderPlugin;
 use select_area::SelectAreaPlugin;
+use solar_system::SolarSystemPlugin;
 
-mod command_defender;
+// mod command_defender;
+mod pancam;
 mod select_area;
+mod solar_system;
 
 const WINDOW_TITLE: &str = "Commander Defender";
 const WINDOW_WIDTH: f32 = 1133.0;
@@ -14,7 +18,12 @@ const WINDOW_HEIGHT: f32 = 744.0;
 const BACKGROUND_COLOR: Color = Color::BLACK;
 
 fn spawn_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle::default()).insert(PanCam {
+        grab_buttons: vec![MouseButton::Middle],
+        min_scale: 1.,
+        max_scale: Some(10.),
+        ..default()
+    });
 }
 
 fn main() {
@@ -28,12 +37,14 @@ fn main() {
             },
             ..default()
         }))
+        .add_plugin(PanCamPlugin::default())
         .add_plugin(MousePositionPlugin)
-        .add_plugin(CommandDefenderPlugin)
+        // .add_plugin(CommandDefenderPlugin)
         .add_plugin(SelectAreaPlugin)
         .insert_resource(WinitSettings::desktop_app())
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .add_startup_system(spawn_camera)
+        .add_plugin(SolarSystemPlugin)
         .add_system(bevy::window::close_on_esc)
         .run();
 }
