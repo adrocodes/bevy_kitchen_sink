e960f48358683e210468aba2d583ab921866205e
adrocodes <adrocodes@users.noreply.github.com>
2023-01-12 20:24:59 +1100

    feat: initial time scale controls

--- src/solar_system.rs
+++ src/solar_system.rs
@@ -16,12 +16,18 @@ struct Sun;
 #[derive(Resource, Default)]
 struct TimeScale(f32);
 
+#[derive(Component)]
+struct TimeScaleUi;
+
 impl Plugin for SolarSystemPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(TimeScale(5.0))
+        app.insert_resource(TimeScale(1.0))
+            .add_startup_system(setup_ui)
             .add_startup_system(spawn_sun)
             .add_startup_system(spawn_planets)
-            .add_system(rotate_planets_around_sun);
+            .add_system(rotate_planets_around_sun)
+            .add_system(button_system)
+            .add_system(time_scale_ui);
     }
 }
 
@@ -199,4 +205,128 @@ fn rotate_planets_around_sun(
             Quat::from_rotation_z(planet.speed * time_scale.0 * time.delta_seconds()),
         );
     }
+}
+
+const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
+const HOVERED_BUTTON: Color = Color::rgb(0.25, 0.25, 0.25);
+const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
+
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut time_scale: ResMut<TimeScale>,
+) {
+    for (interaction, mut color) in &mut interaction_query {
+        match *interaction {
+            Interaction::Clicked => {
+                *color = PRESSED_BUTTON.into();
+                // TODO: Implement decrease
+                time_scale.0 += 1.;
+            }
+            Interaction::Hovered => {
+                *color = HOVERED_BUTTON.into();
+            }
+            Interaction::None => {
+                *color = NORMAL_BUTTON.into();
+            }
+        }
+    }
+}
+
+fn time_scale_ui(
+    node_query: Query<&Children, With<TimeScaleUi>>,
+    mut text_query: Query<&mut Text>,
+    time_scale: Res<TimeScale>,
+) {
+    let children = node_query.single();
+    let mut text = text_query.get_mut(children[0]).unwrap();
+    text.sections[0].value = time_scale.0.to_string();
+}
+
+fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>, time_scale: Res<TimeScale>) {
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                align_items: AlignItems::FlexEnd,
+                justify_content: JustifyContent::FlexEnd,
+                padding: UiRect::all(Val::Px(10.0)),
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(50.0), Val::Px(50.0)),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: NORMAL_BUTTON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "-",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
+        })
+        .with_children(|parent| {
+            parent
+                .spawn((
+                    NodeBundle {
+                        style: Style {
+                            size: Size::new(Val::Px(50.0), Val::Px(50.0)),
+                            justify_content: JustifyContent::Center,
+                            align_items: AlignItems::Center,
+                            ..default()
+                        },
+                        background_color: NORMAL_BUTTON.into(),
+                        ..default()
+                    },
+                    TimeScaleUi,
+                ))
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        time_scale.0.to_string(),
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(50.0), Val::Px(50.0)),
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    background_color: NORMAL_BUTTON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "+",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
+        });
 }
