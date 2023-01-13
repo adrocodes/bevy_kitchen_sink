d4218877d786a0b6121e4668796751344c9f0033
adrocodes <adrocodes@users.noreply.github.com>
2023-01-13 20:31:10 +1100

    feat: add logic to capture selectable entities in area

--- src/select_area.rs
+++ src/select_area.rs
@@ -6,14 +6,20 @@ pub struct SelectAreaPlugin;
 
 #[derive(Component, Default, Debug)]
 struct RegionState {
-    start_ui: Vec2,
-    start_world: Vec2,
-    end_ui: Vec2,
-    end_world: Vec2,
+    /// A tuple containing the UI cursor position (0) and World cursor position (1)
+    start: (Vec2, Vec2),
+    /// A tuple containing the UI cursor position (0) and World cursor position (1)
+    end: (Vec2, Vec2),
 }
 
 #[derive(Component)]
 struct Region;
+
+#[derive(Component)]
+pub struct Selectable;
+
+#[derive(Resource, Debug, Default)]
+pub struct SelectedEntities(Vec<Entity>);
 
 #[derive(Debug)]
 struct ConfirmationEvent {
@@ -23,7 +29,8 @@ struct ConfirmationEvent {
 
 impl Plugin for SelectAreaPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup_tracking_region)
+        app.insert_resource(SelectedEntities::default())
+            .add_startup_system(setup_tracking_region)
             .add_event::<ConfirmationEvent>()
             .add_system(
                 track_region
@@ -61,37 +68,32 @@ fn track_region(
     let (mut region, mut visible) = query.single_mut();
 
     if buttons.just_pressed(MouseButton::Right) {
-        region.start_ui = mouse_position.cursor_ui;
-        region.end_ui = mouse_position.cursor_ui;
-        region.start_world = mouse_position.world;
-        region.end_world = mouse_position.world;
+        region.start = (mouse_position.cursor_ui, mouse_position.world);
+        region.end = (mouse_position.cursor_ui, mouse_position.world);
         visible.is_visible = true;
     }
 
     if buttons.just_released(MouseButton::Right) {
         ev_confirmation.send(ConfirmationEvent {
-            start: region.start_world,
-            end: region.end_world,
+            start: region.start.1,
+            end: region.end.1,
         });
 
-        region.start_ui = Vec2::default();
-        region.end_ui = Vec2::default();
-        region.start_world = Vec2::default();
-        region.end_world = Vec2::default();
+        region.start = (Vec2::default(), Vec2::default());
+        region.end = (Vec2::default(), Vec2::default());
         visible.is_visible = false;
     }
 
     if buttons.pressed(MouseButton::Right) {
-        region.end_ui = mouse_position.cursor_ui;
-        region.end_world = mouse_position.world;
+        region.end = (mouse_position.cursor_ui, mouse_position.world);
     }
 }
 
 fn draw_region(mut query: Query<(&RegionState, &mut Style), With<Region>>) {
     let (region, mut style) = query.single_mut();
 
-    let start: Vec2 = region.start_ui;
-    let end: Vec2 = region.end_ui;
+    let start: Vec2 = region.start.0;
+    let end: Vec2 = region.end.0;
 
     let width = end.x - start.x;
     let height = end.y - start.y;
@@ -101,8 +103,29 @@ fn draw_region(mut query: Query<(&RegionState, &mut Style), With<Region>>) {
     style.size = Size::new(Val::Px(width.abs()), Val::Px(height.abs()));
 }
 
-fn capture_region_confirmation(mut ev_confirmation: EventReader<ConfirmationEvent>) {
+fn capture_region_confirmation(
+    mut ev_confirmation: EventReader<ConfirmationEvent>,
+    query: Query<(Entity, &Transform), With<Selectable>>,
+    mut selected_entities: ResMut<SelectedEntities>,
+) {
+    let mut entities: Vec<Entity> = Vec::new();
+
     for ev in ev_confirmation.iter() {
-        eprintln!("Event {:?}", ev);
+        let max = ev.start.max(ev.end);
+        let min = ev.start.min(ev.end);
+
+        for (entity, transform) in query.into_iter() {
+            let position = transform.translation;
+
+            let in_x_axis = (max.x > position.x) && (min.x < position.x);
+            let in_y_axis = (max.y > position.y) && (min.y < position.y);
+            let in_area = in_x_axis && in_y_axis;
+
+            if in_area {
+                entities.push(entity);
+            }
+        }
     }
+
+    selected_entities.0 = entities;
 }
