0374370d09fe5c7ee47fd720a679ce476d95763e
adrocodes <adrocodes@users.noreply.github.com>
2023-01-25 19:24:14 +1100

    feat: mine spawning function

--- examples/mini_ai.rs
+++ examples/mini_ai.rs
@@ -1,10 +1,70 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 const WINDOW_TITLE: &str = "Mini AI";
 const WINDOW_WIDTH: f32 = 1133.0;
 const WINDOW_HEIGHT: f32 = 744.0;
 
 const BACKGROUND_COLOR: Color = Color::BLACK;
+
+#[derive(Component)]
+struct Mine;
+
+impl Mine {
+    const SIZE: f32 = 24.0;
+    const SIDES: usize = 7;
+    const BORDER_WIDTH: f32 = 3.0;
+    const RADII: f32 = 6.0;
+
+    fn spawn(
+        commands: &mut Commands,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        materials: &mut ResMut<Assets<ColorMaterial>>,
+    ) {
+        let child_size: f32 = Mine::SIZE - Mine::BORDER_WIDTH;
+
+        let inner_hexagon = MaterialMesh2dBundle {
+            mesh: meshes
+                .add(shape::RegularPolygon::new(child_size, Mine::SIDES).into())
+                .into(),
+            material: materials.add(ColorMaterial::from(BACKGROUND_COLOR)),
+            transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
+            ..default()
+        };
+
+        let inner_circle = MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(Mine::RADII).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::WHITE)),
+            transform: Transform::from_translation(Vec3::new(0., 0., 2.)),
+            ..default()
+        };
+
+        commands
+            .spawn((
+                MaterialMesh2dBundle {
+                    mesh: meshes
+                        .add(shape::RegularPolygon::new(Mine::SIZE, Mine::SIDES).into())
+                        .into(),
+                    material: materials.add(ColorMaterial::from(Color::WHITE)),
+                    transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+                    ..default()
+                },
+                Mine,
+            ))
+            .with_children(|parent| {
+                parent.spawn(inner_hexagon);
+                parent.spawn(inner_circle);
+            });
+    }
+}
+
+fn spawn_initial_mines(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    Mine::spawn(&mut commands, &mut meshes, &mut materials);
+    // Mine::spawn(&commands, &meshes, &materials);
+}
 
 fn spawn_camera(mut commands: Commands) {
     // Without Pan Cam
@@ -32,5 +92,7 @@ fn main() {
 struct MiniAiPlugin;
 
 impl Plugin for MiniAiPlugin {
-    fn build(&self, app: &mut App) {}
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_initial_mines);
+    }
 }
